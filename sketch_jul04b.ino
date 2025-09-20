a4575b86888d8bd1bc656b5bd18f554725ce24e5
MohammadMahdi Rezazadeh <43700856+pooya-rz2@users.noreply.github.com>
2025-09-21 02:01:12 +0330

    arduino side

---
+++ sketch_jul04b.ino
@@ -0,0 +1,23 @@
+
+const int buttonPin = 4;
+int buttonState = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+    pinMode(buttonPin, INPUT);
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ buttonState = digitalRead(buttonPin);
+if (buttonState == HIGH) {
+      Serial.print("fesharrrrrrrrrrrrrrrrrrrr");
+}
+else {
+  Serial.print("not yet");
+}
+delay(500);
+
+}
